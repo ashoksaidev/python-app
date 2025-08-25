728a85f2981decbdde39cf25bf11dccb09f2d2b1
ashoksaidev <ashoksaidevops6@gmail.com>
2025-08-25 23:33:03 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,9 @@
+import io
+import sys
+import pytest
+from src.main.python import app
 
+def test_main_output(capsys):
+    app.main()
+    captured = capsys.readouterr()
+    assert "Hello from Python + Gradle!" in captured.out
