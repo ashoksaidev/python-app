8842f72076402d15d572a77a23c6c6ee345f3976
ashoksaidev <ashoksaidevops6@gmail.com>
2025-08-29 11:51:03 +0530

    Update app.py

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from a super-fast POC pipeline!"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
